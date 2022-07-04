4225ed98e9dce6238a99d18a5877235954df75f6
Emilio Rodriguez <jorge.rodriguezv@becarios.infotec.mx>
2022-07-04 00:33:21 -0500

    fix: fix some problems with the route and imports.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import{BrouserRouter as Router,Switch,Route)from'react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Narvar from './components/navegacion/Narvar'
+import Matricula from './components/pages/Matricula'
+import Pokemon from './components/pages/Pokemon'
+import Items from './components/pages/Items'
 
 
 function App(){
-}
+
   return(
     <div className="App">
-      <h1>Navbar</h1>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"></link>
+      <Narvar/>
+    <BrowserRouter>
+<Routes>
+<Route path='/' element={<Pokemon/>}/>
+</Routes>
+         
+      </BrowserRouter>
+
+
     </div>
  );
 
+}
 
 export default App;
